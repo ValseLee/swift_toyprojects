bed770bba0cd060470b4fc64539c01fa08d01656
valseLee <sollleky72@gmail.com>
2022-07-22 11:57:28 +0900

    update: class를 final 키워드로

--- Implement/LoginScreenImpl/LoginScreenImpl/ViewController.swift
+++ Implement/LoginScreenImpl/LoginScreenImpl/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 	private let textHeight = 48.0
 	
 	// Config: emailTextFieldView - basic setting
